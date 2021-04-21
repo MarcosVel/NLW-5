28a487852bafdb02ea7a00d90b1f1d7c50b19751
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-21 19:09:28 -0300

    add states

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
 import { Button } from '../components/Button'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 export function UserIdentification() {
+  const [ isFocused, setIsFocused ] = useState(false);
+  const [ isFilled, setIsFilled ] = useState(false);
+  const [ name, setName ] = useState<string>();
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
 
   return (
     <SafeAreaView style={ styles.container }>
@@ -16,7 +33,9 @@ export function UserIdentification() {
           <View style={ styles.form }>
 
             <View style={ styles.header }>
-              <Text style={ styles.emoji }>😄</Text>
+              <Text style={ styles.emoji }>
+                { isFilled ? '😄' : '🤔'}
+              </Text>
 
               <Text style={ styles.title }>
                 Como podemos{ '\n' }
@@ -25,8 +44,14 @@ export function UserIdentification() {
             </View>
 
             <TextInput
-              style={ styles.input }
+              style={ [
+                styles.input,
+                (isFocused || isFilled) && { borderColor: colors.green }
+              ] }
               placeholder='Digite um nome'
+              onBlur={ handleInputBlur }
+              onFocus={ handleInputFocus }
+              onChangeText={ handleInputChange }
             />
 
             <View style={ styles.footer }>
