ffc09fd58c24cfdf032998d0978f82ecb84757a6
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-22 11:09:10 -0300

    add Routes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import {
   Jost_400Regular,
   Jost_600SemiBold, useFonts
 } from '@expo-google-fonts/jost';
-import { StatusBar } from 'react-native';
-import { Welcome } from './src/pages/Welcome';
 import AppLoading from 'expo-app-loading';
-import { UserIdentification } from './src/pages/UserIdentification';
-import { Confimation } from './src/pages/Confirmation';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -22,9 +20,7 @@ export default function App() {
     <>
       <StatusBar barStyle='dark-content' backgroundColor='transparent'
       />
-      {/* <Welcome /> */}
-      {/* <UserIdentification /> */}
-      <Confimation />
+      <Routes />
     </>
   )
 }
