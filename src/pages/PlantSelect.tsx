445e2ec8527f6ea1a03786e11aad9bc6a463b8dc
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-23 14:21:57 -0300

    styling PlantSelection

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../components/Header';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
   return (
     <View style={ styles.container }>
       <Header />
+
+      <Text style={ styles.title }>Em qual ambiente</Text>
+      <Text style={ styles.subtitle }>você quer colocar sua planta?</Text>
     </View>
   )
 }
@@ -15,6 +19,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
-    
+    paddingHorizontal: 30
+  },
+
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+
+  subtitle: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.text,
+    lineHeight: 20,
   }
 })
