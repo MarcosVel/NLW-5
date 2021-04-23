9ec15ed4d51a8f6df61db543ba13add20e6fc7e3
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-23 14:14:56 -0300

    add Header of user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Header() {
   return (
-    <View style={styles.container }>
-      <Text>
-
-      </Text>
+    <View style={ styles.container }>
+      <View>
+        <Text style={ styles.greeting }>Olá,</Text>
+        <Text style={ styles.userName }>Renata</Text>
+      </View>
+      <Image
+        source={ { uri: 'https://avatars.githubusercontent.com/u/62637265?v=4' } }
+        style={ styles.image }
+      />
     </View>
   )
 }
@@ -18,7 +24,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: 20,
-    backgroundColor: colors.red
+    padding: 20
+  },
+
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  },
+
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
   }
 });
