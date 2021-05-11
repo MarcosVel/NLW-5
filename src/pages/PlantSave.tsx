ab5947abd6f46f3a2ea314c1bf63144535e5a937
MarcosVel <marcos.veloso@sempreceub.com>
2021-05-11 10:35:24 -0300

    storage w/ PlantProps

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -16,20 +16,10 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 import { format, isBefore } from 'date-fns';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { PlantProps } from '../libs/storage';
 
 interface Params {
-  plant: {
-    id: string;
-    name: string;
-    about: string;
-    water_tips: string;
-    photo: string;
-    environments: [ string ];
-    frequency: {
-      times: number;
-      repeat_every: string;
-    }
-  }
+  plant: PlantProps;
 }
 
 export function PlantSave() {
